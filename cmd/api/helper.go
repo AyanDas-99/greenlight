2bc8deb2caeb60425185823347a18a78d05e5cfb
Ayan <ayandas9531@gmail.com>
2025-02-09 14:01:37 +0530

    Filter: movies can be filtered based on query

--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -6,13 +6,54 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
+	"greenlight.ayandas/internal/validator"
 )
 
 type envelope map[string]interface{}
+
+// Return key's value string from query, or the default value
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	return s
+}
+
+// Reads string value from query and splits into slice no the comma. If no matching key found, returns default value
+func (app *application) readCSV(qs url.Values, key string, defaultValue []string) []string {
+	csv := qs.Get(key)
+	if csv == "" {
+		return defaultValue
+	}
+
+	return strings.Split(csv, ",")
+}
+
+// reads string from query and converts to integer.
+//
+// If no value, then return default value
+// and if couldn't convert to integer, record error message in validator
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+
+	return i
+}
 
 // Reads json from body and stores in [dst]
 //
