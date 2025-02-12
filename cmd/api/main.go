f6ac33e6ef6c13d51a41da79637e91b5219543b2
Ayan <ayandas9531@gmail.com>
2025-02-12 23:40:18 +0530

    added user, sending email and graceful shutdown of background processes with wait groups

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,9 +7,13 @@ import (
 	"os"
 	"time"
 
-	_ "github.com/lib/pq"
 	"greenlight.ayandas/internal/data"
 	"greenlight.ayandas/internal/jsonlog"
+	"greenlight.ayandas/internal/mailer"
+
+	"sync"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -28,12 +32,21 @@ type config struct {
 		burst  int
 		enable bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
@@ -53,6 +66,13 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enable, "limiter-enabled", true, "Enable rate limiter")
 
+	// SMTP settings
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "4eff83cbce2b0b", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "277e1499a4af17", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.ayandas.com>", "SMTP sender")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
@@ -70,6 +90,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 	err = app.serve()
 	if err != nil {
